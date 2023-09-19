b7bbf3387b5ace10e57479d0fce06ce00b7fa6f7
HH <275258836@qq.com>
2023-09-19 15:44:49 +0800

    完成Result

---
+++ src/mysql/result.rs
@@ -0,0 +1,24 @@
+use crate::mysql::resultset::ResultSet;
+
+#[derive(Debug, Default, Clone)]
+pub struct Result {
+    pub status: u16,
+    pub warnings: u16,
+    pub insert_id: u64,
+    pub affected_rows: u64,
+    pub result_set: Option<ResultSet>,
+}
+
+impl Result {
+    pub fn close(&mut self) {
+        if !self.result_set.is_none() {
+            self.result_set = None
+        }
+    }
+}
+
+/*
+type Executer interface {
+    Execute(query string, args ...interface{}) (*Result, error)
+}
+ */
